6fbae65c3e8acbc9301c266f3bb4b5eabeb537d1
kisschou <kisschou@me.com>
2020-12-28 16:06:17 +0800

    修改错误回执

--- error.go
+++ error.go
@@ -12,6 +12,8 @@ func (e *Error) GetError(errCode string) (errMsg string) {
 	errMsg = errCode
 	if ConfigLib.Get("error." + errCode).IsExists() {
 		errMsg = ConfigLib.Get("error." + errCode).String()
+	} else {
+		errMsg = ConfigLib.Get("error.ERROR_UNKNOW").String()
 	}
 	return
 }
@@ -20,6 +22,8 @@ func (e *Error) GetErrorCode(errCode string) (code int) {
 	ConfigLib := new(Config)
 	if ConfigLib.Get("error_map." + errCode).IsExists() {
 		code = ConfigLib.Get("error_map." + errCode).Int()
+	} else {
+		code = ConfigLib.Get("error_map.ERROR_UNKNOW").Int()
 	}
 	return
 }
