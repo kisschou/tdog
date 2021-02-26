9cbea221adef8d81948f30c81d5c4550f8284909
kisschou <kisschou@me.com>
2021-02-26 22:07:36 +0800

    添加驼峰、蛇形转换，首字母大写方法

--- util.go
+++ util.go
@@ -347,3 +347,81 @@ func (u *Util) MySQLColumnTypeConvert(columnType string) string {
 	}
 	return convert
 }
+
+/**
+ * 驼峰转蛇形 snake string
+ * @param s 需要转换的字符串
+ * @return string
+ **/
+func (u *Util) SnakeString(s string) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		// or通过ASCII码进行大小写的转化
+		// 65-90（A-Z），97-122（a-z）
+		//判断如果字母为大写的A-Z就在前面拼接一个_
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		data = append(data, d)
+	}
+	//ToLower把大写字母统一转小写
+	return strings.ToLower(string(data[:]))
+}
+
+/**
+ * 蛇形转驼峰
+ * @param s要转换的字符串
+ * @return string
+ */
+func (u *Util) CamelString(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == '_' && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
+
+/**
+ * 首字母转大写
+ * @param s要转换的字符串
+ * @return string
+ **/
+func (u *Util) UcFirst(s string) string {
+	var upperStr string
+	vv := []rune(s)
+	for i := 0; i < len(vv); i++ {
+		if i == 0 {
+			if vv[i] >= 97 && vv[i] <= 122 { // 后文有介绍
+				vv[i] -= 32 // string的码表相差32位
+				upperStr += string(vv[i])
+			} else {
+				return s
+			}
+		} else {
+			upperStr += string(vv[i])
+		}
+	}
+	return upperStr
+}
