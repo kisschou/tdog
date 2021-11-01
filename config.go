f9f6c2132ac1ae5fbf1d55988e9a557320a98158
Kisschou <kisschou@me.com>
2021-11-01 11:52:36 +0800

    统一文稿备注

--- config.go
+++ config.go
@@ -1,8 +1,3 @@
-// Copyright 2012 Kisschou. All rights reserved.
-// Based on the path package, Copyright 2011 The Go Authors.
-// Use of this source code is governed by a MIT-style license that can be found
-// at https://github.com/kisschou/tdog/blob/master/LICENSE.
-
 package tdog
 
 import (
